fed31259b0ac9a9cf3378d12cc32216774a388f3
Macie <macielanthier2005@gmail.com>
2026-05-25 17:29:41 -0400

    Refactor HTML structure, enhance CSS styles, and improve JavaScript functionality for drawing and color selection

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
- let canvas = document.querySelector('.canvas');
+const canvas = document.querySelector('.canvas');
+
 let reset = document.querySelector('.clear-canvas');
+const history = [];
+let isDrawing = false;
 
- 
 
-function css(element, styles) {
+
+function css(element, styles, updateAll) {
     for (let property in styles) {
         element.style[property] = styles[property];
     }
@@ -13,13 +16,32 @@ function css(element, styles) {
 for (let i = 0; i < 256; i++) {
     let squares = document.createElement('div')
     squares.classList.add('square')
-    squares.addEventListener('click', function(e) {
-        css(squares,{
-            backgroundColor: "black"
-        });
-    })
+    
+const defaultColor = "#000000";
+const colorPicker = document.querySelector(".color");
+colorPicker.value = defaultColor;
+     squares.addEventListener('mousedown', function() {
+        isDrawing = true; 
+     })
+     squares.addEventListener('mouseup', function() {
+        isDrawing = false;
+     })
+
+squares.addEventListener('mousemove', function() {
+    if (isDrawing) {
+        if (squares.style.backgroundColor === "") {
+            history.push(squares)
+        }
+        css(squares, { backgroundColor: colorPicker.value })
+
+    }
+})
     canvas.appendChild(squares)
 }
+
+
+
+
 
 // block 2 - reset button
 reset.addEventListener('click', function() {
@@ -30,5 +52,16 @@ reset.addEventListener('click', function() {
         });
     }
 })
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.key === "z") {
+        const lastSquare = history.pop()  
+        if (lastSquare) {
+            css(lastSquare, { backgroundColor: "" })
+        }
+    }
+})
+
   
 
